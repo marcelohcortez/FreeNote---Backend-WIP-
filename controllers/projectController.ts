7df44bb55d1c018a01dbdd12007af4dbdf93803e
marcelohcortez <marcelohcortez@gmail.com>
2024-01-24 18:25:36 +0100

    Prepare controllers

---
+++ controllers/projectController.ts
@@ -0,0 +1,5 @@
+// get all projects
+// get single project
+// create project
+// delete project
+// update project
---
+++ controllers/userController.ts
@@ -0,0 +1,5 @@
+// get all users
+// get single user
+// create user
+// delete user
+// update user
