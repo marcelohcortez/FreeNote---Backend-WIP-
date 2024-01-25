f59ea5cb526e452586dd5d8006f505cd49941443
marcelohcortez <marcelohcortez@gmail.com>
2024-01-25 12:10:36 +0100

    Fix nodemon to work with TypeScript

---
+++ server.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import projectRoutes from "./routes/project";
+
+const app = express();
+
+//routes
+app.get("/", (req, res) => {
+    res.send('Welcome to my server!');
+})
+
+app.use('/api/projects', cors(), projectRoutes);
+
+
+//connect to DB
+process.env.DBURL ? 
+mongoose.connect(process.env.DBURL)
+    .then((result) => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB & listening on port', process.env.PORT)
+        })
+    })
+    .catch((err) => console.log(err))
+    :
+    '';
